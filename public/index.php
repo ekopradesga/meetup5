5709d9ec438e9957a5db512cf13d20e2fe4659cc
Eko Prasetyo <eko@pradesga.com>
2017-05-21 11:39:38 +0700

    update konstanta

--- public/index.php
+++ public/index.php
@@ -3,20 +3,25 @@
  * constant atau konstanta ini adalah static variable ga bisa rubah
  * constant untuk mendifinisikan file project 
  */
+define('DS', DIRECTORY_SEPARATOR);
 define('ABSPATH', str_replace("public", '', $_SERVER['DOCUMENT_ROOT']) );
+define('APPPATH', ABSPATH . "App" . DS);
+define('COREPATH', ABSPATH . "Core" . DS);
 
 /*
  * memasukan file Core\MYApp.php kedalam file ini
  * require, require_once, include, include_once
  */
-require_once(ABSPATH . "Core/MYApp.php");
+require_once(COREPATH . "MYApp.php");
 
 /*
  * ini untuk mengaktifkan object di konsep oop
  */
 $myapp = new MYApp();
+
 /*
  * ini untuk memanggil method dari sebuah object pada konsep oop
  */
 $myapp->start();
+
 ?>
